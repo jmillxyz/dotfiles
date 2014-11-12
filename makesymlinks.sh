33a5498078c1fead4eea20f34a45750af9f3cd39
Jon Miller <jonmiller@miller.talon.net>
2014-11-12 05:44:09 -0600

    Copy files directly instead of symlinking; old dotfiles are already preserved

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -28,7 +28,7 @@ for file in $files; do
   echo "Moving any existing dotfiles from ~/ to $olddir"
   mv ~/.$file ~/dotfiles_old/
   echo "Creating symlink to $file in home directory."
-  ln -s $dir/.$file ~/.$file
+  cp -r $dir/.$file ~/.$file
 done
 
 # Install Vundle first
